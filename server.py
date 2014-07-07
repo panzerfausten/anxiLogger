22254df27cd1505ac0cdbd13836da569e9068869
mc <marco.calderon.perez@gmail.com>
2014-07-07 13:05:58 -0700

    Removed code that was reading values from HTTP request.

--- server.py
+++ server.py
@@ -12,16 +12,15 @@ def loggerData():
 	if request.method == "POST":
 		date = datetime.now();
 		sdate = date.strftime('%Y-%m-%d %H-%M:%S.%f');
-		timestamp = request.form["timestamp"];
-		value = request.form["value"];
-		print timestamp;
-		print value;
-		outputFile.write("%s,%s\n" % (sdate, value));
+		#timestamp = request.form["timestamp"];
+		#value = request.form["value"];
+		print sdate;
+		outputFile.write("%s\n" % (sdate));
 		return "ok";
 	else:
 		app.logger.error('Not a POST method');
 		return "error";
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
